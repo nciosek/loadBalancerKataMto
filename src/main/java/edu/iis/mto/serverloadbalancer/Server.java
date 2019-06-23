8ad489c52c9fca834c50f8fcf8ea10d1e1823e53
Natalia Ciosek <n_ciosek@wp.pl>
2019-06-23 22:16:09 +0200

    test 4 green

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -1,9 +1,14 @@
 package edu.iis.mto.serverloadbalancer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server {
     public double currentLoadPercentage;
     public int capacity;
     private static final double MAXIMUM_LOAD = 100.0d;
+
+    private List<Vm> vms = new ArrayList<Vm>();
 
     public Server(int capacity) {
         this.capacity = capacity;
@@ -15,9 +20,10 @@ public class Server {
 
     public void addVm(Vm vm) {
         currentLoadPercentage = (double)vm.size / (double)capacity * MAXIMUM_LOAD;
+        this.vms.add(vm);
     }
 
     public int countVms() {
-        return 0;
+        return vms.size();
     }
 }
