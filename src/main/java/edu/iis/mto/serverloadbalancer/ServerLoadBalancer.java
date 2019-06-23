3bcfe12c82f03f094fae68ecb1d047a27af4d7e1
Natalia Ciosek <n_ciosek@wp.pl>
2019-06-23 23:18:00 +0200

    test 6 green

--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -1,16 +1,31 @@
 package edu.iis.mto.serverloadbalancer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServerLoadBalancer {
 
 	public void balance(Server[] servers, Vm[] vms) {
 		for (Vm vm : vms) {
-			Server lessLoaded = extractLessLoadedServer(servers);
-			lessLoaded.addVm(vm);
+			addToLessLoadedServer(servers, vm);
 		}
 
 	}
 
-	private Server extractLessLoadedServer(Server[] servers) {
+	private void addToLessLoadedServer(Server[] servers, Vm vm) {
+		List<Server> capableServer = new ArrayList<Server>();
+		for (Server server : servers) {
+			if (server.canFit(vm)){
+				capableServer.add(server);
+			}
+		}
+		Server lessLoaded = extractLessLoadedServer(capableServer);
+		if (lessLoaded != null) {
+			lessLoaded.addVm(vm);
+		}
+	}
+
+	private Server extractLessLoadedServer(List<Server> servers) {
 		Server lessLoaded = null;
 		for(Server server : servers){
 			if(lessLoaded == null || lessLoaded.currentLoadPecentage > server.currentLoadPecentage){
--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -72,7 +72,7 @@ public class ServerLoadBalancerTest {
 	@Test
 	public void test6(){
 		Server theServer = a(server().withCapacity(10).withCurrentLoadOf(90.0d));
-		Vm theVm = a(vm().ofSize(1));
+		Vm theVm = a(vm().ofSize(2));
 		balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
 
 		assertThat("the server should not contain vm", !theServer.contains(theVm));
