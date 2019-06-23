62948e3fb3d27cd4c3684265e2ee8b036dfeb397
Natalia Ciosek <n_ciosek@wp.pl>
2019-06-23 22:26:42 +0200

    test 5 green

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerBuilder.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerBuilder.java
@@ -2,6 +2,7 @@ package edu.iis.mto.serverloadbalancer;
 
 public class ServerBuilder implements Builder<Server> {
     private int capacity;
+    private double initialLoad;
 
     public ServerBuilder withCapacity(int capacity) {
         this.capacity = capacity;
@@ -9,7 +10,13 @@ public class ServerBuilder implements Builder<Server> {
     }
 
     public Server build() {
-        return new Server(capacity);
+        final Server server = new Server(capacity);
+        if (initialLoad > 0) {
+            int initialVmSize = (int) (initialLoad / (double) capacity * 100.0d);
+            Vm initialVm = VmBuilder.vm().ofSize(initialVmSize).build();
+            server.addVm(initialVm);
+        }
+        return server;
     }
 
     public static ServerBuilder server() {
@@ -17,6 +24,7 @@ public class ServerBuilder implements Builder<Server> {
     }
 
     public ServerBuilder withLoadPercentage(double initialLoad) {
-        return null;
+        this.initialLoad = initialLoad;
+        return this;
     }
 }
