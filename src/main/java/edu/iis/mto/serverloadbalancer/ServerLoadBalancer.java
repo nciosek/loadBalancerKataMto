8db1929459c29b55d4d6d8b338ebd20a70b97c1f
Natalia Ciosek <n_ciosek@wp.pl>
2019-06-24 08:19:56 +0200

    test5 green

--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -3,8 +3,15 @@ package edu.iis.mto.serverloadbalancer;
 public class ServerLoadBalancer {
     public void balance(Server[] servers, Vm[] vms) {
         for (Vm vm : vms) {
-            servers[0].addVm(vms[0]);
+            Server lessLoadedServer = null;
+            for (Server server : servers) {
+                if (lessLoadedServer == null || server.hasCurrentPercentage < lessLoadedServer.hasCurrentPercentage){
+                    lessLoadedServer = server;
+                }
+            }
+            lessLoadedServer.addVm(vm);
         }
+
 
     }
 }
