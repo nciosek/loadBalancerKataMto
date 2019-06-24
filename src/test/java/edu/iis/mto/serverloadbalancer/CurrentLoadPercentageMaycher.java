d038a0ac898bdc90ab9ec10a2bc3d027c4002a52
Natalia Ciosek <n_ciosek@wp.pl>
2019-06-24 23:10:08 +0200

    test1 green

---
+++ src/test/java/edu/iis/mto/serverloadbalancer/CurrentLoadPercentageMaycher.java
@@ -0,0 +1,21 @@
+package edu.iis.mto.serverloadbalancer;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class CurrentLoadPercentageMaycher extends TypeSafeMatcher<Server> {
+    private double expectedLoadPercentage;
+
+    public CurrentLoadPercentageMaycher(double expectedLoadPercentage) {
+        this.expectedLoadPercentage = expectedLoadPercentage;
+    }
+
+    protected boolean matchesSafely(Server server) {
+        return expectedLoadPercentage == server.currentLoadPercentage || Math.abs(expectedLoadPercentage - server.currentLoadPercentage) < 100.0d;
+    }
+
+    public void describeTo(Description description) {
+        description.appendText("server with load percentage of").appendValue(expectedLoadPercentage);
+    }
+}
