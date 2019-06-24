2f6fa0db4c10f49e5f357986ccde829738e9579c
Natalia Ciosek <n_ciosek@wp.pl>
2019-06-24 08:04:57 +0200

    test3 refactor

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -3,6 +3,7 @@ package edu.iis.mto.serverloadbalancer;
 public class Server {
     public double hasCurrentPercentage;
     public int capacity;
+    private static final double MAXIMUM_LOAD = 100.0d;
 
     public Server(int capacity) {
         this.capacity = capacity;
@@ -10,5 +11,9 @@ public class Server {
 
     public boolean contains(Vm theVm) {
         return true;
+    }
+
+    public void addVm(Vm vm) {
+        hasCurrentPercentage = (double)vm.size / (double)capacity * MAXIMUM_LOAD;
     }
 }
--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -3,7 +3,7 @@ package edu.iis.mto.serverloadbalancer;
 public class ServerLoadBalancer {
     public void balance(Server[] servers, Vm[] vms) {
         for (Vm vm : vms) {
-            servers[0].hasCurrentPercentage = (double)vms[0].size / (double)servers[0].capacity * 100.0d;
+            servers[0].addVm(vms[0]);
         }
 
     }
