42bc0e8e22d53b520f4e1b70e886107118e28d62
Natalia Ciosek <n_ciosek@wp.pl>
2019-06-24 23:32:22 +0200

    test4

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -45,6 +45,22 @@ public class ServerLoadBalancerTest {
 		assertThat("the server should conatin the vm", theServer.contains(theVm));
 	}
 
+	@Test
+	public void test4(){
+		Server theServer = a(server().withCapacity(100));
+		Vm theFirstVm = a(vm().ofSize(1));
+		Vm theSecondVm = a(vm().ofSize(1));
+		balancing(aServersListWith(theServer), aVmsListWith(theFirstVm, theSecondVm));
+
+		assertThat(theServer, aVmsCountOf(2));
+		assertThat("the server should conatin the first vm", theServer.contains(theFirstVm));
+		assertThat("the server should conatin the second vm", theServer.contains(theSecondVm));
+	}
+
+	private Matcher<? super Server> aVmsCountOf(int expectedCountVm) {
+		return new ServerVmsCountMatcher(expectedCountVm);
+	}
+
 	private Vm[] aVmsListWith(Vm... vms) {
 		return vms;
 	}
