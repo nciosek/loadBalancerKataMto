930186a453679d0847a8e11fe5b5ed4a53e84cec
Natalia Ciosek <n_ciosek@wp.pl>
2019-06-25 01:15:42 +0200

    test6 red

--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -1,5 +1,8 @@
 package edu.iis.mto.serverloadbalancer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServerLoadBalancer {
     public void balance(Server[] servers, Vm[] vms) {
         for (Vm vm : vms) {
@@ -8,7 +11,14 @@ public class ServerLoadBalancer {
     }
 
     private void addToLessLoadedServer(Server[] servers, Vm vm) {
-        Server lessLoadedServer = findLessLoadedServer(servers, vm);
+        List<Server> capableServer = new ArrayList<Server>();
+        for (Server server : servers) {
+            if (server.canFit(vm)){
+                capableServer.contains(server);
+            }
+        }
+
+        Server lessLoadedServer = findLessLoadedServer(capableServer);
         lessLoadedServer.addVm(vm);
     }
 
