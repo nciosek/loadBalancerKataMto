a3efb4086e7fb36e04dc46894b3b744cda11d648
Natalia Ciosek <n_ciosek@wp.pl>
2019-06-25 12:41:32 +0200

    test6

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -69,6 +69,15 @@ public class ServerLoadBalancerTest {
 		assertThat("more loaded server should not contain the vm", !moreLoadedServer.contains(theVm));
 	}
 
+	@Test
+	public void test6(){
+		Server theServer = a(server().withCapacity(10).withCurrentLoad(90.0d));
+		Vm theVm = a(vm().ofSize(2));
+		balancing(aServersListWith(theServer), aVmsListWith(theVm));
+
+		assertThat("server should not contain the vm", !theServer.contains(theVm));
+	}
+
 	private Vm[] aVmsListWith(Vm... vms) {
 		return vms;
 	}
