7aece0b0e1cde70863c779b3082889eff24d0fb9
Natalia Ciosek <n_ciosek@wp.pl>
2019-06-25 12:21:38 +0200

    test3 green

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -2,6 +2,11 @@ package edu.iis.mto.serverloadbalancer;
 
 public class Server {
     public double currentLoadPercentage;
+    public int capacity;
+
+    public Server(int capacity) {
+        this.capacity = capacity;
+    }
 
     public boolean contains(Vm theVm) {
         return true;
