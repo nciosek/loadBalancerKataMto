3f64c02b58a11c346a3f240ee2c0bd626089d565
Natalia Ciosek <n_ciosek@wp.pl>
2019-06-25 12:37:47 +0200

    test5 ref

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerBuilder.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerBuilder.java
@@ -11,12 +11,16 @@ public class ServerBuilder implements Builder<Server> {
 
     public Server build() {
         final Server server = new Server(capacity);
+        findInitialLoad(server);
+        return server;
+    }
+
+    private void findInitialLoad(Server server) {
         if (initialLoad > 0) {
-            int initialVmsSize = (int) (initialLoad / (double) capacity * 100.0d);
+            int initialVmsSize = (int) (initialLoad / (double) capacity * server.MAXIMUM_LOAD);
             Vm initialVms = VmBuilder.vm().ofSize(initialVmsSize).build();
             server.addVm(initialVms);
         }
-        return server;
     }
 
     public static ServerBuilder server() {
