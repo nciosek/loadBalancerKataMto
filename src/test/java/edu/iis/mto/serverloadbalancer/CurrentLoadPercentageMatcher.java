a788b45f666c253fb650376ae73c88ef92ca1da5
Natalia Ciosek <n_ciosek@wp.pl>
2019-06-25 07:56:11 +0200

    test1 green

---
+++ src/test/java/edu/iis/mto/serverloadbalancer/CurrentLoadPercentageMatcher.java
@@ -0,0 +1,21 @@
+package edu.iis.mto.serverloadbalancer;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class CurrentLoadPercentageMatcher extends TypeSafeMatcher<Server> {
+    private double expectedLoadPercenatge;
+
+    public CurrentLoadPercentageMatcher(double expectedLoadPercenatge) {
+        this.expectedLoadPercenatge = expectedLoadPercenatge;
+    }
+
+    protected boolean matchesSafely(Server server) {
+        return expectedLoadPercenatge == server.currentLoadPercentage || Math.abs(expectedLoadPercenatge - server.currentLoadPercentage) < 0.01d;
+    }
+
+    public void describeTo(Description description) {
+        description.appendText("the server load percentage of").appendValue(expectedLoadPercenatge);
+    }
+}
